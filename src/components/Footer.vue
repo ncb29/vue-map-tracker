6b44c8181c9df5c9330e0ee75905ebb5f5265e63
ncb29 <Kontakt@nilsbarth.de>
2024-01-31 19:38:28 +0100

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,46 +1,46 @@
 <script>
-  import { clientGeoData }  from './ClientsGeoData.js'
+    import { clientGeoData }  from './ClientsGeoData.js'
 
-  export default {       
-      el: '#footer',
-      name: "Footer",
-      methods: {
-          clientGeoData,
-          // getClientsGeoDataFooter() {
-              
-          // }
-      },
-      data() {
-          return {
-              renderFooter: true,
-          };
-      },
-      created: function () {
-          clientGeoData();
-          // this.getClientsGeoDataFooter.call(this);
-      },
-      mounted() {
-          const that = this;        
+    export default {       
+        el: '#footer',
+        name: "Footer",
+        methods: {
+            clientGeoData,
+            // getClientsGeoDataFooter() {
+                
+            // }
+        },
+        data() {
+            return {
+                renderFooter: true,
+            };
+        },
+        created: function () {
+            clientGeoData();
+            // this.getClientsGeoDataFooter.call(this);
+        },
+        mounted() {
+            const that = this;        
 
-          this.emitter.on("update-components", () => {                     
-              setTimeout(function () {
-                  parseFooterContent.call(that);
-              }.bind(this), 1000);
-          });
+            this.emitter.on("update-components", () => {                     
+                setTimeout(function () {
+                    parseFooterContent.call(that);
+                }.bind(this), 1000);
+            });
 
-          setTimeout(function () {
-              parseFooterContent.call(this);
-          }.bind(this), 1000);  
+            setTimeout(function () {
+                parseFooterContent.call(this);
+            }.bind(this), 1000);  
 
-          function parseFooterContent() {
-              let oCurrentPosition =  window.localStorage.getItem("ClientsCurrentPosition")      
-              oCurrentPosition = JSON.parse(oCurrentPosition);
-              console.log("FOOTER COMPONENT Local Storage Item", oCurrentPosition);
-              this.$el.innerHTML = "<p>"+oCurrentPosition.latitude+" - "+oCurrentPosition.longitude+"</p> <p>Genauigkeit: "+oCurrentPosition.accuracy+" Meter</p>";
-              this.$forceUpdate();
-          }          
-      }
-  }
+            function parseFooterContent() {
+                let oCurrentPosition =  window.localStorage.getItem("ClientsCurrentPosition")      
+                oCurrentPosition = JSON.parse(oCurrentPosition);
+                console.log("FOOTER COMPONENT Local Storage Item", oCurrentPosition);
+                this.$el.innerHTML = "<p>"+oCurrentPosition.latitude+" - "+oCurrentPosition.longitude+"</p> <p>Genauigkeit: "+oCurrentPosition.accuracy+" Meter</p>";
+                this.$forceUpdate();
+            }          
+        }
+    }
 </script>
 
 <template>
