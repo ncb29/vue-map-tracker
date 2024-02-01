cbec7bb84c8bb162848d7dc865f710a0b8d8c21b
ncb29 <Kontakt@nilsbarth.de>
2024-02-01 18:09:03 +0100

    added comments

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Implement a emitter to refresh components data from other components
 import mitt from 'mitt';
 const emitter = mitt();
 const app = createApp(App);
 app.config.globalProperties.emitter = emitter;
+
+// Mount the app
 app.mount('#app');
