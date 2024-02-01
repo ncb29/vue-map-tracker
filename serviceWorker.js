e99a1435519a271780b2022bc6f7017beac6fb8a
ncb29 <Kontakt@nilsbarth.de>
2024-02-01 23:16:54 +0100

    cacheFiles for localhost and web

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,14 +1,30 @@
 // the cache version gets updated every time there is a new deployment
-const CACHE_VERSION = 2;
+const CACHE_VERSION = 4;
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
+const cacheFiles = []
 
 // these are the routes we are going to cache for offline support
-const cacheFiles = [
-    './index.html',
-    './', // Alias for index.html
-    './assets/js/index.js',
-    './assets/css/index.css'
-];
+if (location.hostname !== "localhost" || location.hostname !== "127.0.0.1") {
+    const cacheFiles = [
+        './index.html',
+        './', // Alias for index.html
+        './assets/js/index.js',
+        './assets/css/index.css'
+    ];
+} else {
+    const cacheFiles = [
+      './index.html',
+      './', // Alias for index.html
+      './src/App.js',
+      './src/App.vue',
+      './src/assets/*.css',
+      './src/assets/*.svg',
+      './src/assets/gifs/*.gif',
+      './src/assets/icons/*.png',
+      './src/components/*.vue',
+      './src/components/*.js',
+  ];
+}
 
 // on activation we clean up the previously registered service workers
 self.addEventListener('activate', evt =>
