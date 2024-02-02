9ece5f584e91200db697c94913b468c78d0a6e72
ncb29 <Kontakt@nilsbarth.de>
2024-02-02 23:41:20 +0100

    removed unnecessary that context, added info boxes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,5 @@
 <script>
+    // import { ref } from 'vue'
 
     export default {       
         el: '#footer',
@@ -13,20 +14,30 @@
 
         },
         mounted() {
-            const that = this;        
 
             this.emitter.on("update-components", () => {                     
-                renderFooterContent.call(that);
+                renderFooterContent.call(this);
             });
 
             function renderFooterContent() {
                 let oStoredCurrentPosition = JSON.parse( window.localStorage.getItem("ClientsCurrentPosition"));
 
                 if (oStoredCurrentPosition !== null && (Object.keys(oStoredCurrentPosition).length !== 0 && oStoredCurrentPosition.constructor === Object)) {
-                    this.$el.innerHTML = "<p>"+oStoredCurrentPosition.latitude+" - "+oStoredCurrentPosition.longitude+"</p> <p>Genauigkeit: "+oStoredCurrentPosition.accuracy+" Meter</p>"; 
+
+                    const footerInnerHTML = "<p>"+oStoredCurrentPosition.latitude+" - "+oStoredCurrentPosition.longitude+"</p> <p>Genauigkeit: "+oStoredCurrentPosition.accuracy+" Meter</p>";
+
+                    if (oStoredCurrentPosition.message === "") {
+                        this.$el.childNodes[0].innerHTML = footerInnerHTML; 
+                        // this.$el.childNodes[1].innerHTML = ""; 
+                    } else {
+                        // If error message exist render it in message box
+                        this.$el.childNodes[0].innerHTML = footerInnerHTML; 
+                        // this.$el.childNodes[1].innerHTML = oStoredCurrentPosition.message; 
+                    }
+                    
                 } else {
                     // Set fallback geo data
-                    this.$el.innerHTML = "<p>53.5560767 - 9.9284123</p> <p>Genauigkeit: 0 Meter</p>";
+                    this.$el.childNodes[0].innerHTML = "<p>53.5560767 - 9.9284123</p> <p>Genauigkeit: 0 Meter</p>";
                 }   
                 console.log("FOOTER COMPONENT Local Storage Item", oStoredCurrentPosition);                
                 this.$forceUpdate();
@@ -36,5 +47,8 @@
 </script>
 
 <template>
-    <div class="app__main-container--footer" id="footer" v-if="renderFooter">0</div >
+    <div class="app__main-container--footer" id="footer" v-if="renderFooter">
+        <div class="app__main-container--footer-geoData">0</div>
+        <!-- <div class="app__main-container--footer-messages">0</div> -->
+    </div>
 </template>
