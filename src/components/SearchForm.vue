b23e4f1a207d2cd6af8df501c90b5de55eb6b758
ncb29 <Kontakt@nilsbarth.de>
2024-02-27 18:10:27 +0100

    adjustement of class names

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -55,14 +55,19 @@
 
 <template>
     <div class='app__main-container--search' v-bind:class='{ showSearch: isSearchOpen }'>
-        <div class='app__main-container--search-form'>
-            <input placeholder='Ort suchen' id="searchInput" ref="searchInput"/>
+        <form action='' class='app__main-container--search-form'>
+            <div class='app__main-container--search-form-input'>
+                <input placeholder='Ort suchen' id="searchInput" ref="searchInput"/>
+                <input type="reset" value="X">
+            </div>               
             <svg class='app__main-container--search-submit svgSpriteBox' @click='submitSearch()'>
                 <use xlink:href='#searchGlass'></use>
             </svg>
-        </div>
+        </form>            
         <div class='app__main-container--search-close' @click='closeSearch()'>
-            X
+            <svg class='app__main-container--search-submit svgSpriteBox'>
+                <use xlink:href='#closeCircle'></use>
+            </svg>
         </div>            
     </div>    
     <div class='app__main-container--searchToggle' @click='toggleSearch()' v-bind:class='{ hideSearchToggle: isSearchOpen }'>
