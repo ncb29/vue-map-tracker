5baa6cc98eb1b29e3271612958f11cfeacfe150e
ncb29 <Kontakt@nilsbarth.de>
2024-02-29 23:44:37 +0100

    new layout with header, toggle disabled submit button

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -10,6 +10,7 @@
         data: () => ({ 
             isSearchOpen: false,
             isRoutingOpen: false,
+            isSubmitDisabled: true
         }),
 
         methods: {
@@ -17,9 +18,49 @@
             getGeoSearchData,
             getGeoRoutingData,
 
-            resetSearchValue () {
+            resetSearchValue ( inputType ) {
+
                 // The input is cleared automaticly by form reset button.
-                this.$refs.searchInput.focus();
+                if ( inputType === 'search' ) {
+                    
+                    this.$refs.searchInput.value = '';
+                    this.$refs.searchInput.focus();
+
+                } else if ( inputType === 'routing-start' ) {
+
+                    this.$refs.routingInputStart.value = '';
+                    this.$refs.routingInputStart.focus();
+
+                } else {
+
+                    this.$refs.routingInputEnd.value = '';
+                    this.$refs.routingInputEnd.focus();
+                }
+
+                this.toggleDisableSubmitButton.call( this );
+            },
+
+            toggleDisableSubmitButton () {
+                const searchInputValueLength = this.$refs.searchInput.value.length;
+                const routingStartValueLength = this.$refs.routingInputStart.value.length;
+                const routingEndValueLength = this.$refs.routingInputEnd.value.length;
+
+                console.log("searchInputValue", searchInputValueLength)
+
+                if ( this.isRoutingOpen === false ) {
+                    if ( searchInputValueLength > 0 ) {
+                        this.isSubmitDisabled = false;
+                    } else {
+                        this.isSubmitDisabled = true;
+                    }
+                } else {
+
+                    if ( routingStartValueLength > 0 && routingEndValueLength > 0 ) {
+                        this.isSubmitDisabled = false;
+                    } else {
+                        this.isSubmitDisabled = true;
+                    }
+                }
             },
 
             async submitSearchOrRouting () {
@@ -59,6 +100,7 @@
 
 
             toggleSearch () {
+
                 this.emitter.emit( 'add-custom-map-class' );  
                 this.isSearchOpen = true;
 
@@ -66,7 +108,9 @@
                     this.$refs.searchInput.focus();
                 } else {
                     this.$refs.routingInputStart.focus();
-                }                
+                }
+                
+                this.toggleDisableSubmitButton.call( this );
             },
 
 
@@ -84,6 +128,8 @@
                 } else {
                     this.$refs.routingInputStart.focus();
                 }   
+
+                this.toggleDisableSubmitButton.call( this );
             },
 
             
@@ -96,7 +142,7 @@
         },
 
         created () {
-            
+           
         },
 
         beforeMount () {
@@ -105,44 +151,70 @@
 
         mounted () { 
 
+            const searchFormInputs = [this.$refs.searchInput, this.$refs.routingInputStart, this.$refs.routingInputEnd]
+
+            searchFormInputs.forEach( function ( input ) {
+                
+                input.addEventListener("keyup", 
+
+                    function () {
+                        this.toggleDisableSubmitButton.call( this );
+                    }.bind( this )
+                );
+            }.bind( this ))            
         }
     }
 </script>
 
-<template>
+<template>  
     <div class='app__main-container--search' v-bind:class='{ showSearch: isSearchOpen }, { routingIsOpen: isRoutingOpen }'>
+        <div class='app__main-container--search-header'> 
+            <div class='app__main-container--search-header-titleBox'>
+                <h3 class='app__main-container--search-header-titleBox-title' v-bind:class='{ hideTitle: isRoutingOpen }'>
+                    Suche Lokation
+                </h3>
+                <h3 class='app__main-container--search-header-titleBox-title' v-bind:class='{ hideTitle: !isRoutingOpen }'>
+                    Suche Route
+                </h3>
+                <svg class='app__main-container--search-form-toggleRouting svgSpriteBox' v-bind:class='{ hideSearchTypeIcon: isRoutingOpen }' @click='toggleRouting()'>
+                    <use xlink:href='#routing'></use>
+                </svg>  
+                <svg class='app__main-container--search-form-toggleRouting svgSpriteBox' v-bind:class='{ hideSearchTypeIcon: !isRoutingOpen }' @click='toggleRouting()'>
+                    <use xlink:href='#magnifyingGlass'></use>
+                </svg>  
+            </div>            
+            <div class='app__main-container--search-close' @click='closeSearch()'>
+                <svg class='svgSpriteBox'>
+                    <use xlink:href='#closeCircle'></use>
+                </svg>
+            </div> 
+        </div>          
         <form action='' class='app__main-container--search-form'>
             <div class='app__main-container--search-form-inputBox'>
                 <div class='app__main-container--search-form-inputBox-input app__main-container--search-form-inputBox-location' v-bind:class='{ hideSearch: isRoutingOpen }'>
                     <input placeholder='Ort suchen' id="searchInput" ref="searchInput"/>
-                    <input type="reset" value="X" @click='resetSearchValue()'>
+                    <input type="button" value="X" @click='resetSearchValue( "search" )'>
                 </div>  
                 <div class='app__main-container--search-form-inputBox-routing' v-bind:class='{ showRouting: isRoutingOpen }'>
                     <div class='app__main-container--search-form-inputBox-input'>
                         <input placeholder='Start eingeben' id="routingInputStart" ref="routingInputStart"/>
-                        <input type="reset" value="X" @click='resetSearchValue()'>
+                        <input type="button" value="X" @click='resetSearchValue( "routing-start" )'>
                     </div>
                     <div class='app__main-container--search-form-inputBox-input'>
                         <input placeholder='Ziel eingeben' id="routingInputEnd" ref="routingInputEnd"/>
-                        <input type="reset" value="X" @click='resetSearchValue()'>
+                        <input type="button" value="X" @click='resetSearchValue( "routing-end" )'>
                     </div>
                 </div>                   
                 <!-- <svg class='app__main-container--search-form-inputBox-toggleRouting svgSpriteBox' @click='switchRoutingPoints()'>
                     <use xlink:href='#switch'></use>
                 </svg>  -->
-            </div>    
-            <svg class='app__main-container--search-form-toggleRouting svgSpriteBox' @click='toggleRouting()'>
-                    <use xlink:href='#routing'></use>
-                </svg>         
-            <svg class='app__main-container--search-form-submit svgSpriteBox' @click='submitSearchOrRouting()'>
-                <use xlink:href='#searchGlass'></use>
-            </svg>
-        </form>            
-        <div class='app__main-container--search-close' @click='closeSearch()'>
-            <svg class='svgSpriteBox'>
-                <use xlink:href='#closeCircle'></use>
-            </svg>
-        </div>            
+            </div> 
+            <button class='app__main-container--search-form-submit btn btn--icon' @click='submitSearchOrRouting()' :disabled='isSubmitDisabled'>
+                <svg class='svgSpriteBox'>
+                    <use xlink:href='#searchGlass'></use>
+                </svg>
+            </button>                          
+        </form>           
     </div>    
     <div class='app__main-container--searchToggle' @click='toggleSearch()' v-bind:class='{ hideSearchToggle: isSearchOpen }'>
         <svg class='svgSpriteBox'>
